09728fbe4281cec2fd4d627524afb155676b7bcc
Iacopo Ruzzier <iacopo@alessandroruzzier.it>
2023-11-13 00:12:19 +0100

    Aggiunta animazione walk e funzione set_animation()

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,12 @@
 extends KinematicBody2D
 
 export var speed := 100
+export var fall_speed = 100
 var velocity: Vector2
+var input_direction
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
 func _physics_process(delta):
@@ -12,6 +14,17 @@ func _physics_process(delta):
 	get_input()
 	
 	if not is_on_floor():
-		velocity.y = 100
+		velocity.y = fall_speed
 	
 	move_and_slide(velocity)
+	
+	set_animation()
+
+
+#gestione animazioni
+func set_animation() -> void:
+	if input_direction.length() > 0 :
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_h = input_direction.x < 0
+	else:
+		$AnimatedSprite.animation = "idle"
